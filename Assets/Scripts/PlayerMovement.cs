ea5785f776143e87cdd4df3d8fef02ea983d0856
Eric Grogans <SlyCedix@gmail.com>
2020-08-07 09:24:54 -0400

    Reimplement player controller as rigid body
    Known Bug:
    Interacting with a slope with the opposing side of the capsule collider (usually when player rotation is near 0), the player gets a large upward velocity

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,50 +1,83 @@
-using System.Collections;
-using System.Collections.Generic;
-using System.Runtime.CompilerServices;
+
 using UnityEngine;
-using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(CapsuleCollider))]
+[RequireComponent(typeof(Rigidbody))]
 
 public class PlayerMovement : MonoBehaviour
 {
-    private PlayerControls _playerControls;
-    private CharacterController _characterController;
-    private Camera _camera;
+    #region Member Variables
+    [SerializeField] private float _baseSpeed = 5f;
+    [SerializeField] private float _sprintMultiplier = 1.5f;
+    [SerializeField] private float _crouchMultiplier = 0.5f;
+    [SerializeField] private float _reverseMultiplier = 0.5f;
 
+    [SerializeField] private float _jumpForce = 4.5f;
+    [SerializeField] private float _gravity = 9.81f;
+    [SerializeField] private float _terminalVelocity = -50f;
+    [SerializeField] private float _maxUpwardVelocity = 150f;
+    [SerializeField] private float _airAcceleration = 1f;
+
+    [SerializeField, Range(0f, 90f)] private float _slopeLimit = 45f;
+    [SerializeField, Range(0f, 1f)] private float _frictionMultiplier = 0.95f;
+    [SerializeField] private float _minSpeed = 0.1f;
+
+    private float _speedMultiplier = 1f;
+
+    private bool _sprinting = false;
+    private bool _crouching = false;
+
+    [SerializeField] private bool _isGrounded = false;
+    [SerializeField] private Vector3 _groundVector = Vector3.up;
+
+    private PlayerControls _playerControls;
+    private Rigidbody _rigidbody;
+    private CapsuleCollider _collider;
+    #endregion
+
+    #region Monobehaviour
     private void Awake()
     {
         _playerControls = new PlayerControls();
-        _characterController = GetComponent<CharacterController>();
-        _camera = Transform.FindObjectOfType<Camera>();
+        _rigidbody = GetComponent<Rigidbody>();
+        _collider = GetComponent<CapsuleCollider>();
+
+        _playerControls.Default.Jump.started += context => Jump();
+        _playerControls.Default.Sprint.started += context => SprintStarted();
+        _playerControls.Default.Sprint.canceled += context => SprintCanceled();
+        _playerControls.Default.Crouch.started += context => CrouchStarted();
+        _playerControls.Default.Crouch.canceled += context => CrouchCanceled();
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
+        Gravity();
         Locomotion();
+        Friction();
+        _isGrounded = false;
+        _groundVector = _isGrounded ? _groundVector : transform.up;
     }
 
-    private void Locomotion()
+    private void OnCollisionStay(Collision collision)
     {
+        var contacts = new ContactPoint[collision.contactCount];
+        collision.GetContacts(contacts);
+        _groundVector = transform.up;
 
-    }
+        foreach (var contact in contacts)
+        {
+            var bottom = _collider.bounds.center - Vector3.up * _collider.bounds.extents.y;
+            var curve = bottom + (Vector3.up * _collider.radius);
+            var angle = Vector3.Angle(contact.normal, transform.up);
 
-    private void SprintStarted()
-    {
-    }
+            Debug.DrawLine(curve, contact.point, Color.blue, 0.5f);
 
-    private void SprintCanceled()
-    {
-    }
-
-    private void CrouchStarted()
-    {
-    }
-
-    private void CrouchCanceled()
-    {
-    }
-
-    private void Jump()
-    {
+            if ((Mathf.Abs(angle) <= _slopeLimit))
+            {
+                _isGrounded = true;
+                _groundVector = contacts[0].normal;
+            }
+        }
     }
 
     private void OnEnable()
@@ -56,4 +89,112 @@ public class PlayerMovement : MonoBehaviour
     {
         _playerControls.Disable();
     }
+    #endregion
+
+    #region Member Methods
+    private void Locomotion()
+    { 
+        var movement = _playerControls.Default.Move.ReadValue<Vector2>() * _baseSpeed * _speedMultiplier;
+        var rotation = Quaternion.FromToRotation(transform.up, _groundVector);
+        var hVel = new Vector3(movement.x, 0.0f, movement.y);
+        hVel = rotation * hVel;
+        hVel = transform.rotation * hVel;
+        hVel += transform.up * _rigidbody.velocity.y;
+
+        if(hVel.y > 0.7f)
+        {
+           Debug.Log(hVel);
+        }
+
+        if (movement.y <= 0)
+        {
+            movement *= _reverseMultiplier;
+            SprintCanceled();
+        }
+
+        if (_isGrounded)
+        {
+            _rigidbody.velocity = hVel;
+        } else
+        {
+            var airAcceleration = movement.normalized * _airAcceleration;
+
+            _rigidbody.AddRelativeForce(new Vector3(airAcceleration.x, 0f, airAcceleration.y), ForceMode.Acceleration);
+        }
+    }
+
+    private void Gravity()
+    {
+        _rigidbody.AddForce(_groundVector * -_gravity, ForceMode.Acceleration);
+
+        var velocity = _rigidbody.velocity;
+        velocity.y = Mathf.Clamp(_rigidbody.velocity.y, _terminalVelocity, _maxUpwardVelocity);
+
+        _rigidbody.velocity = velocity;
+    }
+
+    private void Friction()
+    {
+        if (_isGrounded)
+        {
+            var hVelocity = _rigidbody.velocity;
+            hVelocity.y = 0f;
+
+            if (hVelocity.magnitude <= _minSpeed)
+            {
+                hVelocity = Vector3.zero;
+            }
+            else
+            {
+                hVelocity *= _frictionMultiplier;
+            }
+
+            _rigidbody.velocity = new Vector3(hVelocity.x, _rigidbody.velocity.y, hVelocity.z);
+        }
+    }
+
+    private void Jump()
+    {
+        if (_isGrounded)
+        {
+            _rigidbody.AddForce(transform.up * _jumpForce, ForceMode.VelocityChange);
+        }
+    }
+
+    private void SprintStarted()
+    {
+        if (!_sprinting)
+        {
+            _sprinting = true;
+            _speedMultiplier *= _sprintMultiplier;
+        }
+    }
+
+    private void SprintCanceled()
+    {
+        if (_sprinting)
+        {
+            _sprinting = false;
+            _speedMultiplier /= _sprintMultiplier;
+        }
+    }
+
+    private void CrouchStarted()
+    {
+        if (!_crouching)
+        {
+            _crouching = true;
+            _speedMultiplier *= _crouchMultiplier;
+        }
+    }
+
+    private void CrouchCanceled()
+    {
+        if (_crouching)
+        {
+            _crouching = false;
+            _speedMultiplier /= _crouchMultiplier;
+        }
+    }
+    #endregion
 }
